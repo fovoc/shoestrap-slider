22593bd4f30fced26a933472cf6916f043cd7401
Aristeides Stathopoulos <aristath@gmail.com>
2013-10-10 12:22:13 +0300

    initial commit

---
+++ loader.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Shoestrap Slider
+Plugin URI: http://wpmu.io
+Description: Transforms WordPress Galleries
+Version: 1.00
+Author: Aristeides Stathopoulos
+Author URI: http://aristeides.com
+*/
+
+require_once dirname( __FILE__ ) . '/includes/wp_bootstrap_gallery.php';
